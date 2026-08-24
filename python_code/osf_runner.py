f7aff7073a10ef81e43dc87f9a9219befe93e3e4
CodeJoris <joristheil.ecole@gmail.com>
2026-08-24 11:28:42 -0400

    feat: support for kielmat + refactoring

--- python_code/osf_runner.py
+++ python_code/osf_runner.py
@@ -1,3 +1,23 @@
+'''
+Module: osf_runner.py
+
+This script serves as the main entry point for processing the OSF Stair 
+Ambulation dataset. It extracts the target sequences, prepares the 
+data, and processes it using the PyShoe toolbox. The script supports 
+parallel processing to speed up the workflow.
+
+dependencies:
+- gaitmap-datasets
+- pandas
+- numpy
+- re
+- pathlib
+- concurrent.futures
+- tqdm
+- python_code.src.preprocessing.stair_ambulation_extraction
+- python_code.Toolboxes.PyShoe.pyshoe_export
+'''
+
 import pandas as pd
 import numpy as np
 import re
@@ -6,11 +26,28 @@ from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
 from functools import partial
 
-from python_code.src.preprocessing.stair_ambulation_extraction import extract_ascending_stairs_data
+from python_code.src.preprocessing.osf_surface_extraction import extract_surface_segments
 from python_code.Toolboxes.PyShoe.pyshoe_export import pyshoe_process_single_file
+from python_code.Toolboxes.KielMAT.kielmat_export import kielmat_process_single_file
 
 DATA_PATH = Path(__file__).resolve().parent.parent / "data"
-OUTPUT_DIR = DATA_PATH / "ambulation_out"
+TARGET_MODE = "stair_up"  # CHANGE THIS: to see other surfaces 
+#eg. 'stairs_down' gets you all segments with atleast 'stairs' and 'down' in the test name.
+
+'''
+surface names:
+{'stair_flat_up_slow', 'staircase_up_fast', 'stair_long_flying_normal', 
+'stair_long_up_fast', 'staircase_up_normal', 'stair_long_down_single_step', 
+'stair_long_up_normal', 'stair_long_down_double_step', 'stair_long_down_fast', 
+'stair_flat_down_fast', 'stair_long_down_slow', 'stair_long_up_double_step', 
+'staircase_down_fast', 'stair_long_up_slow', 'stair_long_up_single_step', 
+'staircase_up_slow', 'stair_flat_up_fast', 'stair_flat_down_slow', 
+'slope_descending_normal', 'staircase_down_normal', 'slope_ascending_normal', 
+'stair_flat_up_normal', 'staircase_down_slow', 'stair_long_down_normal', 
+'staircase_flying_normal', 'stair_flat_down_normal'}
+'''
+
+OUTPUT_DIR = DATA_PATH / f'osf_{TARGET_MODE}'  # Change this to your desired output directory"
 
 def process_single_test(test_data, toolbox_dir):
     """Worker function to be executed by ProcessPoolExecutor."""
@@ -25,7 +62,7 @@ def process_single_test(test_data, toolbox_dir):
 
     # Early Exit Check
     already_processed = True
-    for detector in ['shoe']:
+    for detector in ['shoe', 'kielmat']:
         if not (toolbox_dir / detector / f"{subject_id}_{course}_{clip_id}.mat").exists():
             already_processed = False
             break
@@ -35,12 +72,15 @@ def process_single_test(test_data, toolbox_dir):
     # Prepare Data
     sensors = test_data['sensors']
     gt = test_data['ground_truth']
+    hip_acc, hip_gyr = sensors['hip_sensor']['acc_array'], sensors['hip_sensor']['gyr_array']
     acc_l, gyr_l = sensors['left_sensor']['acc_array'], sensors['left_sensor']['gyr_array']
     acc_r, gyr_r = sensors['right_sensor']['acc_array'], sensors['right_sensor']['gyr_array']
     min_len = min(len(acc_l), len(acc_r))
     
     segment_df = pd.DataFrame({
         "time": np.arange(min_len) * (1000.0 / fs),
+        "acceleration_Pelvis_x": hip_acc[:min_len, 0], "acceleration_Pelvis_y": hip_acc[:min_len, 1], "acceleration_Pelvis_z": hip_acc[:min_len, 2],
+        "angularVelocity_Pelvis_x": hip_gyr[:min_len, 0], "angularVelocity_Pelvis_y": hip_gyr[:min_len, 1], "angularVelocity_Pelvis_z": hip_gyr[:min_len, 2],
         "acceleration_LeftFoot_x": acc_l[:min_len, 0], "acceleration_LeftFoot_y": acc_l[:min_len, 1], "acceleration_LeftFoot_z": acc_l[:min_len, 2],
         "angularVelocity_LeftFoot_x": gyr_l[:min_len, 0], "angularVelocity_LeftFoot_y": gyr_l[:min_len, 1], "angularVelocity_LeftFoot_z": gyr_l[:min_len, 2],
         "acceleration_RightFoot_x": acc_r[:min_len, 0], "acceleration_RightFoot_y": acc_r[:min_len, 1], "acceleration_RightFoot_z": acc_r[:min_len, 2],
@@ -53,15 +93,26 @@ def process_single_test(test_data, toolbox_dir):
     true_fo_r = (tc[tc[:, 1] == 0][:, 0] * (1000.0 / fs)) if len(tc) > 0 else np.array([])
     true_fo_l = (tc[tc[:, 1] == 1][:, 0] * (1000.0 / fs)) if len(tc) > 0 else np.array([])
 
-    return pyshoe_process_single_file(
-        segment_df, True, course, subject_id, clip_id, true_hs_r, true_fo_r, true_hs_l, true_fo_l, toolbox_dir, fs
+    pyshoe_result = pyshoe_process_single_file(
+        segment_df, True, course, subject_id, clip_id, 
+        true_hs_r, true_fo_r, 
+        true_hs_l, true_fo_l, 
+        toolbox_dir, fs
+    )
+    kielmat_result = kielmat_process_single_file(
+        segment_df, course, subject_id, clip_id, 
+        true_hs_r, true_fo_r, 
+        true_hs_l, true_fo_l, 
+        toolbox_dir, fs
     )
 
+    return f"{test_name} => PyShoe [{pyshoe_result}] | KielMAT [{kielmat_result}]"
+
 if __name__ == "__main__":
-    NILSPOD_DATA_PATH = DATA_PATH / "stair_ambulation_dataset"
+    NILSPOD_DATA_PATH = DATA_PATH / "osf"
     
     if NILSPOD_DATA_PATH.exists():
-        ascending_tests = extract_ascending_stairs_data(str(NILSPOD_DATA_PATH))
+        ascending_tests = extract_surface_segments(str(NILSPOD_DATA_PATH), TARGET_MODE)
         
         # Use partial to pass the toolbox_dir argument
         worker_func = partial(process_single_test, toolbox_dir=OUTPUT_DIR)
